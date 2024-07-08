c355bfd9e7489dc5300c99fb77fc9cb951ef2c6e
frakhlystov <alex.khlystov@gmail.com>
2024-07-08 15:50:00 +0800

    intitial

---
+++ src/main/java/info/fa/console/Main.java
@@ -0,0 +1,14 @@
+package info.fa.console;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    
+    
+    public static void main(String []args) {
+        //add products
+        //check total price
+        log.info("test");
+    }
+}
